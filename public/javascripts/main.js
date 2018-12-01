2e7da36d76a4287a46bf20986cf0c707e653c6fb
aacm13 <00354817@uca.edu.sv>
2018-12-01 15:44:35 -0600

    trabajando en el controllers

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,5 +4,22 @@ let app = {
     },
     addEvents= function(){
         fetch("/serie")
+            .then(res=>res.json())
+            .then(data=>{
+                let series=document.getElementsByClassName("series")[0];
+                series.innerHTML = data.reduce((cadena, element)=>{
+                    return cadena +
+                        `<tr>
+                            <td class="id">${element.id}</td>
+                            <td class="name">${element.nombre}</td>
+                            <td class="genero">${element.genero}</td>
+                            <td class="capitulos">${element.capitulos}</td>
+                            <td class="options">
+                                <a class="edit"><i class="fas fa-edit"></i></a>
+                                <a class="delete"><i class="fas fa-minus-circle"></i></a>
+                            </td>
+                            `
+                })
+            })
     }
 }
