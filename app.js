ba882743ef6ac82cd48bd9f2ec749d574233af66
aacm13 <00354817@uca.edu.sv>
2018-12-01 16:50:39 -0600

    cambios en app

--- app.js
+++ app.js
@@ -4,25 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-//modelos
+
+//Modelos
 var serie = require('./models/Serie');
 
+// Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var serieRouter = require('./routes/serie');
 
-//connect to mongo
+// Connect to mongo 
 var user = process.env.USERDB || "";
 var password = process.env.PASSDB || "";
 var server = process.env.SERVER || "localhost";
 var db = process.env.DATABASE || "parcial";
 var string = `mongodb://${user}:${password}@${server}/${db}`;
+
 mongoose.Promise = global.Promise;
+
 mongoose.connect(string, {
-  useNewUrlParser:true
-})
-.then(() => console.log('connection successful'))
-.catch(() => console.error('connection faild'));
+    useNewUrlParser: true
+  })
+  .then(() => console.log('Conecction Success'))
+  .catch(() => console.error('No Connected'));
 
 var app = express();
 
@@ -32,21 +36,23 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('serie', serieRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/serie', serieRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
